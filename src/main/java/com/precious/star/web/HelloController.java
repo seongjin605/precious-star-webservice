db0c47316f767fa1a3f8634393f6ec98ea7bff3b
preciouStar <seongjin605@users.noreply.github.com>
2020-01-27 01:57:12 +0900

    create table and save post with h2 database

--- src/main/java/com/precious/star/web/HelloController.java
+++ src/main/java/com/precious/star/web/HelloController.java
@@ -1,12 +1,18 @@
 package com.precious.star.web;
 
+import com.precious.star.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
     @GetMapping("/hello")
-        public String hello() {
-            return "hello";
-        }
+    public String hello() {
+        return "hello";
     }
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount")int amount) {
+        return new HelloResponseDto(name, amount);
+    }
+}

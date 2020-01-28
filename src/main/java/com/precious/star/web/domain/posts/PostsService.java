34153831e9246af4fc868b07ec0af2000d61082f
preciouStar <seongjin605@users.noreply.github.com>
2020-01-29 01:06:40 +0900

    create update service and unit test

--- src/main/java/com/precious/star/web/domain/posts/PostsService.java
+++ src/main/java/com/precious/star/web/domain/posts/PostsService.java
@@ -1,6 +1,8 @@
 package com.precious.star.web.domain.posts;
 
+import com.precious.star.web.dto.PostsResponseDto;
 import com.precious.star.web.dto.PostsSaveRequestDto;
+import com.precious.star.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,5 +16,20 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                                        .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다."));
+        return new PostsResponseDto(entity);
     }
 }
--- src/test/java/com/precious/star/web/web/PostsApiControllerTest.java
+++ src/test/java/com/precious/star/web/web/PostsApiControllerTest.java
@@ -3,13 +3,16 @@ package com.precious.star.web.web;
 import com.precious.star.web.domain.posts.Posts;
 import com.precious.star.web.domain.posts.PostsRepository;
 import com.precious.star.web.dto.PostsSaveRequestDto;
+import com.precious.star.web.dto.PostsUpdateRequestDto;
 import org.junit.After;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -38,15 +41,15 @@ public class PostsApiControllerTest {
     }
 
     @Test
-    public void savePosts() throws Exception {
+    public void savePost() throws Exception {
         String title = "title";
         String content = "content";
 
         PostsSaveRequestDto postsSaveRequestDto = PostsSaveRequestDto.builder()
-                                                                        .title(title)
-                                                                        .content(content)
-                                                                        .author("psj")
-                                                                        .build();
+                .title(title)
+                .content(content)
+                .author("psj")
+                .build();
 
         String url = "http://localhost:" + port + "/api/v1/posts";
 
@@ -57,6 +60,39 @@ public class PostsApiControllerTest {
         List<Posts> all = postsRepository.findAll();
         assertThat(all.get(0).getTitle()).isEqualTo(title);
         assertThat(all.get(0).getContent()).isEqualTo(content);
+
+    }
+
+    @Test
+    public void updatePost() throws Exception {
+        Posts savedPosts = postsRepository.save(Posts.builder()
+                                                            .title("title")
+                                                            .content("content")
+                                                            .author("author")
+                                                            .build());
+
+        Long willUpdateId = savedPosts.getId();
+        String expectedTitle = "title2";
+        String expectedContent = "content2";
+
+        PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder()
+                                                                        .title(expectedTitle)
+                                                                        .content(expectedContent)
+                                                                        .build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts/" + willUpdateId;
+        HttpEntity<PostsUpdateRequestDto> requestDtoHttpEntity = new HttpEntity<>(requestDto);
+
+        // when
+        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestDtoHttpEntity, Long.class);
+
+        // then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(requestDto.getTitle());
+        assertThat(all.get(0).getContent()).isEqualTo(requestDto.getContent());
 
     }
 }

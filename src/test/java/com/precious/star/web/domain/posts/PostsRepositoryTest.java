5c3af101f394289a93e6ee9a4bdad3f2d286dddf
preciouStar <seongjin605@users.noreply.github.com>
2020-02-02 19:06:24 +0900

    Create JPA auditing and unit test

--- src/test/java/com/precious/star/web/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/precious/star/web/domain/posts/PostsRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -29,15 +30,35 @@ public class PostsRepositoryTest {
         String content = "Test Content";
 
         postsRepository.save(Posts.builder()
-                                            .title(title)
-                                            .content(content)
-                                            .author(("test123@naver.com"))
-                                            .build());
+                .title(title)
+                .content(content)
+                .author(("test123@naver.com"))
+                .build());
 
         List<Posts> postsList = postsRepository.findAll();
 
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void saveBaseTimeEntity() {
+        LocalDateTime now = LocalDateTime.of(2020, 2, 2, 0, 0, 0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("contents")
+                .author("author")
+                .build());
+
+        List<Posts> posts = postsRepository.findAll();
+
+        Posts post = posts.get(0);
+
+        System.out.println("createdDate=" + post.getCreatedDate());
+        System.out.println("modifiedDate=" + post.getCreatedDate());
+
+        assertThat("createdDate=" + post.getCreatedDate().isAfter(now));
+        assertThat("modifiedDate=" + post.getModifiedDate().isAfter(now));
     }
 }

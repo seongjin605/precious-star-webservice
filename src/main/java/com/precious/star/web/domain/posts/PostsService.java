2e6ae22dd5666b8e6dc1bbda078e04d57a3a40c4
preciouStar <seongjin605@users.noreply.github.com>
2020-02-03 22:30:41 +0900

    Create list of posts

--- src/main/java/com/precious/star/web/domain/posts/PostsService.java
+++ src/main/java/com/precious/star/web/domain/posts/PostsService.java
@@ -1,11 +1,15 @@
 package com.precious.star.web.domain.posts;
 
+import com.precious.star.web.dto.PostsListResponseDto;
 import com.precious.star.web.dto.PostsResponseDto;
 import com.precious.star.web.dto.PostsSaveRequestDto;
 import com.precious.star.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -21,7 +25,7 @@ public class PostsService {
     @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
         Posts posts = postsRepository.findById(id)
-                                        .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
         posts.update(requestDto.getTitle(), requestDto.getContent());
 
@@ -31,5 +35,13 @@ public class PostsService {
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다."));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc()
+                .stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }

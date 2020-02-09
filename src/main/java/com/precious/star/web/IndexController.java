7d496ab56563305ba1e2eed0c13cb0fcb12adeab
preciouStar <seongjin605@users.noreply.github.com>
2020-02-09 17:43:07 +0900

    Clean code by creating a LoginUser annotation class.

--- src/main/java/com/precious/star/web/IndexController.java
+++ src/main/java/com/precious/star/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.precious.star.web;
 
+import com.precious.star.web.config.auth.LoginUser;
 import com.precious.star.web.config.auth.dto.SessionUser;
 import com.precious.star.web.domain.posts.PostsService;
 import com.precious.star.web.domain.user.User;
@@ -19,12 +20,10 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if(user != null) {
             model.addAttribute("userName", user.getName());

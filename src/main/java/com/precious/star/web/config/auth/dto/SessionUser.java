3d1c892b033045968f5a84fc8699a4314e67d5ca
preciouStar <seongjin605@users.noreply.github.com>
2020-02-09 17:20:05 +0900

    Create OAuth2 for Google Login

---
+++ src/main/java/com/precious/star/web/config/auth/dto/SessionUser.java
@@ -0,0 +1,19 @@
+package com.precious.star.web.config.auth.dto;
+
+import com.precious.star.web.domain.user.User;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getEmail();
+    }
+}
